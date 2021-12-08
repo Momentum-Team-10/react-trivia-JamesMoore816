82ee0c330308e3ba8768661e0d6606eb87aba849
James Moore <james816am@gmail.com>
2021-12-08 16:07:33 -0500

    Data passed to buttons in a way that SEEMS to work

--- react-trivia/src/components/CategoryButton.js
+++ react-trivia/src/components/CategoryButton.js
@@ -3,23 +3,22 @@ import axios from 'axios'
 
 export default function CategoryButton({categoryView, setCategoryView, questionView, setQuestionView}) {
     const categoriesUrl = 'https://opentdb.com/api_category.php'
-    // const {name, id} = props
     const [category, setCategory] = useState([])
 
     useEffect(() => {
         axios
             .get(categoriesUrl)
             .then((response) => {
-                console.log(response.data)
-                setCategory(response.data.trivia_categories.map((category) => category.name))
+                console.log(response.data.trivia_categories.map((category) => [category.name, category.id]))
+                setCategory(response.data.trivia_categories.map((category) => [category.name, category.id]))
+                console.log(category)
             })
     }, [])
 
     return (
         <>
-        {category.map((name, index) => (
-        <button className="btn-sm controls" key={index+9} onClick={() => {setCategoryView(false); setQuestionView(true);}}>{name} {index}</button>
-        
+        {category.map((category) => (
+        <button className="btn-sm controls" key={category[1]} onClick={() => {setCategoryView(false); setQuestionView(true);}}>{category[0]}</button>
         ))}
         </>
     )
